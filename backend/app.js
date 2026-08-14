8da73e4de37ff785e53bc10f34c99df7ddaf4633
sonalbhayani <bhayanisonal84@gmail.com>
2026-08-14 23:04:26 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,9 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
 }));
 connectDB();
