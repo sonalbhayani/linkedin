a076505ffdf29186067d35d3fb5931db5562c1ef
sonalbhayani <bhayanisonal84@gmail.com>
2026-08-27 23:36:12 +0530

    debugg

--- backend/controller/user.contoller.js
+++ backend/controller/user.contoller.js
@@ -12,6 +12,7 @@ const getAuthUser = async (req, res) => {
 const updateProfile = async (req, res) => {
     let data = {};
     data.userId = req.userId;
+    console.log(req);
     let { firstName, lastName, headline, location, gender, skills, education, experience } = req.body;
     data.firstName = firstName;
     data.lastName = lastName;
